d259898afb0227d1a57ffe68b35b73982521b1a2
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2025-09-28 17:56:31 -0500

    428. Personalizando la Validación del DTO

--- posnest/src/categories/dto/create-category.dto.ts
+++ posnest/src/categories/dto/create-category.dto.ts
@@ -1,6 +1,6 @@
-import { IsString } from "class-validator";
+import { IsNotEmpty } from "class-validator";
 
 export class CreateCategoryDto {
-    @IsString()
+    @IsNotEmpty({ message: 'Name should not be empty' })
     name: string;
 }

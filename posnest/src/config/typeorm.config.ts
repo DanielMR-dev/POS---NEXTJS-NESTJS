b1bfba430d69e311d571c819f694edbb3db54b85
DanielMR-dev <80842775+DanielMR-dev@users.noreply.github.com>
2025-12-25 13:17:48 -0500

    432. Agregando las credenciales en variables de Entorno

--- posnest/src/config/typeorm.config.ts
+++ posnest/src/config/typeorm.config.ts
@@ -1,5 +1,13 @@
+import { ConfigService } from "@nestjs/config";
 import type { TypeOrmModuleOptions } from "@nestjs/typeorm";
 
-export const typeOrmConfig = () : TypeOrmModuleOptions => ({
+export const typeOrmConfig = (configService: ConfigService) : TypeOrmModuleOptions => ({
     type: 'postgres',
+    host: configService.get<string>('DATABASE_HOST'),
+    port: configService.get<number>('DATABASE_PORT'),
+    username: configService.get<string>('DATABASE_USER'),
+    password: configService.get<string>('DATABASE_PASS'),
+    database: configService.get<string>('DATABASE_NAME'),
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
 })
